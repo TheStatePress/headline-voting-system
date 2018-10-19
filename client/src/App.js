fce3d4a696bf8a28fbce0d6d39269baf3f9f568f
Chuck Dries <chuck@chuckdries.com>
2018-10-18 21:17:20 -0700

    mostly http based api, to be moved to socket soon

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import urljoin from 'url-join';
+import axios from 'axios';
 import './App.css';
+import { apiURL } from './config.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      headlineInput: '',
+      headlines: {}
+    }
+    this.getHeadlines = this.getHeadlines.bind(this);
+    this.submitHeadline = this.submitHeadline.bind(this);
+  }
+
+  componentDidMount() {
+    this.getHeadlines();
+  }
+
+  getHeadlines() {
+    console.log('here')
+    axios.get(urljoin(apiURL, '/headlines'))
+      .then(headlines => {
+        this.setState({
+          headlines: headlines.data
+        })
+        console.log('asdf')
+        console.log(headlines);
+      })
+  }
+
+  submitHeadline(e) {
+    e.preventDefault();
+    axios.post(urljoin(apiURL, '/headlines'), {
+      headline: this.state.headlineInput,
+      author: 'chuckdries'
+    }).then(() => this.getHeadlines());
+
+    this.setState({
+      headlineInput: ''
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Stale Mess Headline Voting System</h1>
         </header>
+        <div className="headline-area">
+          <ul>
+            {Object.keys(this.state.headlines).map(key => (
+              <li key={key}>{this.state.headlines[key].headline}</li>
+            ))}
+          </ul>
+        </div>
+        <form onSubmit={this.submitHeadline}>
+          <input value={this.state.headlineInput} onChange={(e) => this.setState({ headlineInput: e.target.value })} type="text" name="headline"></input>
+          <input type="submit" value="submit" />
+        </form>
       </div>
     );
   }
